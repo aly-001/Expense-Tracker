b5e5e214a6b328f06d832cb920619c66b48204ad
Alexander Mackenzie <alexandermackenzie3775@gmail.com>
2023-05-25 16:04:14 -0400

    Add input validation to Form component.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form"
 
+interface FormData{
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -11,18 +16,20 @@ const Form = () => {
           Name
         </label>
         <input
-          { ...register('name') }
+          { ...register('name', {required: true, minLength: 3}) }
           type="text"
           id="name"
           className="form-control"
         />
+        {errors.name?.type === 'required' && <p className="text-danger">The name field is required.</p>}
+        {errors.name?.type === 'minLength' && <p className="text-danger">The name must be at least 3 characters.</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register('age')}
+          {...register('age', {required: true})}
           type="number"
           id="age"
           className="form-control"
